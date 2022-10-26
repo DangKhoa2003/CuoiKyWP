337205fa8a0718a5d88fc72a0b26f290f7f2c155
Trần Hà Đăng Khoa <92510208+DangKhoa2003@users.noreply.github.com>
2022-10-26 14:29:47 +0700

    khoaook

--- wp-login.php
+++ wp-login.php
@@ -97,7 +97,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://dangkhoa2003.github.io/CuoiKyWP/wp-includes/js/wp-util.min.js?ver=6.0.3" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"e1509191dc"};
+var userProfileL10n = {"user_id":"0","nonce":"48223afea2"};
 /* ]]> */
 </script>
 <script type="text/javascript" src="https://dangkhoa2003.github.io/CuoiKyWP/wp-admin/js/user-profile.min.js?ver=6.0.3" id="user-profile-js"></script>
